2e85a1cf7f71e1dfadb41dd45ddc433e01498bd5
Agustin LLados <agustinllados@gmail.com>
2024-02-20 00:13:24 -0300

    tratando de conectar con heroku 3 intento-

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json());
 app.use(middleware);
 
-const PORT = process.env.PORT  || 3001;
+const PORT = process.env.PORT  || 8001;
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
